2d703f9c1653770eabb292a56ff053c34352bb86
marlexx <urosvidakovic002@gmail.com>
2023-05-25 14:03:19 +0200

    comment with needed

--- cypress/POM/hoverDemo.js
+++ cypress/POM/hoverDemo.js
@@ -1,6 +1,6 @@
 export class Hover{
     constructor(){
-
+        this.buttons = '.p-15 > .ml-15.flex > div';
     }
 
     hoverButton(buttonNbr){
@@ -8,6 +8,8 @@ export class Hover{
     }
 
     assertColorOfButton(buttonNbr){
-
+        // cy.get(this.buttons).eq(buttonNbr-1).invoke('css', 'background-color').then((color)=>{
+        //     cy.log(color);
+        // })
     }
 }
